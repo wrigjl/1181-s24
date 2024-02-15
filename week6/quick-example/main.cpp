c0bcabbecc9239ec54875e7f8a283a42ff4f363d
Jason L. Wright <jason@thought.net>
2024-02-15 08:28:01 -0700

    add quick example and rock paper scissors

---
+++ week6/quick-example/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int something = 4;
+
+    if (something == 4) {
+        while (true) {
+            if (false) {
+
+            }
+        }
+    }
+    return 0;
+}
---
+++ week6/rps/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+// Ask user for their choice
+char getUserChoice() {
+    char choice;
+    cout << "Weapon choice? r==rock, s==scissors, p==paper";
+    cin >> choice;
+    if (choice == 'r' || choice == 'p' || choice == 's') {
+        return choice;
+    } else {
+        cout << "Yo, wth? that's not valid." << endl;
+        return getUserChoice();
+    }
+}
+
+// randomly generate the computer's choice from the set "r", "p", "s"
+char getComputerChoice() {
+    int num = rand() % 3;
+    if (num == 0)
+        return 'r';
+    if (num == 1)
+        return 'p';
+    return 's';
+}
+
+bool gameWasTied(char userChoice, char computerChoice) {
+    return userChoice == computerChoice;
+}
+
+// Play a game of RPS, return true if the user won, false if
+// the computer won. If there was a tie, play again and return
+// that result.
+
+bool playGame() {
+    char userChoice = getUserChoice();
+    char computerChoice = getComputerChoice();
+
+    cout << "User: " << userChoice << " Computer: " << computerChoice << endl;
+
+    if (gameWasTied(userChoice, computerChoice)) {
+        return playGame();
+    } else if ((userChoice == 'r' && computerChoice == 's') ||
+               (userChoice == 's' && computerChoice == 'p') ||
+               (userChoice == 'p' && computerChoice == 'r')) {
+        // user wins
+        cout << "User wins!" << endl;
+        return true;
+    } else {
+        cout << "User loses!" << endl;
+        return false;
+    }
+}
+
+int playGames(int gamesLeft) {
+    int numberOfUserWins;
+
+    if (gamesLeft == 0) {
+        return 0;
+    }
+    bool userWon = playGame();
+    numberOfUserWins = playGames(gamesLeft - 1);
+    if (userWon) {
+        numberOfUserWins = numberOfUserWins + 1;
+    }
+    return numberOfUserWins;
+}
+int main() {
+    cout << playGames(5) << endl;
+}
