837c1267e8d92690cddb2d1c30dad4f71ee7cf1b
Jason L. Wright <jason@thought.net>
2024-02-23 08:40:31 -0700

    update week7

--- week7/password/main.cpp
+++ week7/password/main.cpp
@@ -8,9 +8,26 @@ int MINIMUM_PASSWORD_LENGTH = 12;
 // Check passwords:
 // Do they have lowercase letters?
 // Do they have uppercase letters?
-// Does it have digits?
+// Does it have at least 2 digits?
 // Does it have special characters?
 // does it contain "password"?
+
+bool hasTwoDigits(string str) {
+    int len = str.length();
+    int i = 0;
+    int numberOfDigits = 0;
+
+    while (i < len) {
+        if (isdigit(        str[i]        )) {
+            numberOfDigits = numberOfDigits + 1;
+        }
+        i = i + 1;
+    }
+    if (numberOfDigits >= 2) {
+        return true;
+    }
+    return false;
+}
 
 bool hasLowerCase(string str) {
     int len = str.length();
@@ -32,7 +49,7 @@ bool hasUpperCase(string str) {
 
     while (i < len) {
         if (isupper(str[i])) {
-            // found a lowercase letter
+            // found a uppercase letter
             return true;
         }
         i = i + 1;
@@ -48,17 +65,37 @@ int main() {
     cout << "You said: " << password << endl;
 
     bool isLongEnough = false;
+    bool lowercaseFound = false;
+    bool uppercaseFound = false;
+    bool twoDigitsFound = false;
 
     // Are they long enough? (12 characters or more)
     if (password.length() >= MINIMUM_PASSWORD_LENGTH) {
         cout << "long enough" << endl;
         isLongEnough = true;
     }
+    // Does it have lowercase letters?
     if (hasLowerCase(password)) {
         cout << "Yes, has lowercase." << endl;
+        lowercaseFound = true;
     }
 
-    // Does it have lowercase letters?
+    if (hasUpperCase(password)) {
+        cout << "Yes, has uppercase." << endl;
+        uppercaseFound = true;
+    }
+
+    if (hasTwoDigits(password)) {
+        cout << "Yes, 2 digits." << endl;
+        twoDigitsFound = true;
+    }
+
+    if (isLongEnough && uppercaseFound && lowercaseFound) {
+        cout << "Your password is strong!" << endl;
+    } else {
+        cout << "WEAK!" << endl;
+    }
+
     cout << "Your password is " << password.length() << " characters long" << endl;
 
     return 0;
